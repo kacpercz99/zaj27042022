d40370f82cf5c240c937bd4caf113412f1b02f5e
KapiCytryna <kacperczura9@gmail.com>
2022-04-27 19:05:40 +0200

    Lekkie zmiany w zad6

--- src/pl/kul/Zad6.java
+++ src/pl/kul/Zad6.java
@@ -1,35 +1,51 @@
 package pl.kul;
+
+import java.util.Arrays;
 import java.util.Random;
 
 import static java.util.Arrays.*;
 
 public class Zad6 {
     public static void zad6() {
-        int[][] macierz = new int[5][5];
+        zad6ZParametrami(5,-5, 15);
+    }
+
+    public static void zad6ZParametrami(int n, int min, int max) {
         Random rand = new Random(System.currentTimeMillis());
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                macierz[i][j] = -5 + rand.nextInt(21);
+        //tworze macierz n x n
+        int[][] macierz = new int[n][n];
+
+        //wypełniam utworzoną macierz losowymi elementami od min do max
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                macierz[i][j] = min + rand.nextInt(max - min + 1);
             }
         }
 
-        System.out.println("Wylosowana macierz 5 x 5:");
-
+        //wypisuje całą macierz
+        System.out.printf("Wylosowana macierz %d x %d:\n",n,n);
         stream(macierz).forEach(row -> {
-            stream(row).forEach(element -> System.out.print(element + " "));
+            stream(row).forEach(element -> System.out.printf("%-4d", element));
             System.out.println();
         });
 
-        int[] maximums = {-6, -6, -6, -6, -6};
-        int[] minimums = {16, 16, 16, 16, 16};
+        //tworze tablice na minima i maxima
+        int[] maximums = new int[n];
+        int[] minimums = new int[n];
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        //wypełniam tablice, odpowiednimi wartościami, aby można je było poprawnie porównywać
+        fill(maximums, min);
+        fill(minimums, max);
+
+        //szukam minimów i maximów dla każdej kolumny
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (macierz[j][i] < minimums[i]) minimums[i] = macierz[j][i];
                 if (macierz[j][i] > maximums[i]) maximums[i] = macierz[j][i];
             }
         }
 
+        //wypisuje minima i maxima dla każdej kolumny
         System.out.println("Minima dla każdej kolumny:");
         stream(minimums).forEach(e -> System.out.print(e + " "));
         System.out.println("\nMaxima dla każdej kolumny:");
