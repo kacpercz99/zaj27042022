db3f9bc06b2f10b1770f59b401e1e71b0af7dfd9
KapiCytryna <kacperczura9@gmail.com>
2022-04-27 16:55:17 +0200

    implemented finding max in 2d array

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,23 @@ public class Main {
         }
     }
 
+    public static int findMaxIn2DArray(int[][] tab) {
+        int maxX = 0;
+        int maxY = 0;
+        int max = tab[0][0];
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                if (tab[i][j] > max) {
+                    max = tab[i][j];
+                    maxX = i;
+                    maxY = j;
+                }
+            }
+        }
+        System.out.printf("Max w tablicy: %d\n Wiersz: %d\n Kolumna: %d\n", max, maxY + 1, maxX + 1);
+        return max;
+    }
+
     public static void main(String[] args) {
         //1)Pobierz liczbę elementów nz klawiatury.
         // Utwórz tablicę o wymiarze n x n.
@@ -43,5 +60,6 @@ public class Main {
         // c. Oblicz, ile liczb jest większych od średniej elementów tablicy
         int[][] tablica = utworzTablice2D();
         printTab2D(tablica);
+        findMaxIn2DArray(tablica);
     }
 }
